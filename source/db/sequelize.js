3c43b43b7995d81f3cb424113f164de361e98745
Alfred <alfred@laptopvonalfred.speedport.ip>
2024-06-24 16:42:33 +0200

    ok

--- source/db/sequelize.js
+++ source/db/sequelize.js
@@ -4,7 +4,7 @@ const admin = require("../models/admin")
 require("dotenv").config()
 
 
-const Db = new Sequelize(process.env.POSTGRES_URLV)
+const Db = new Sequelize(process.env.POSTGRES_URLV_URL)
 const articles = artikels(Db, DataTypes)
 const admins = admin(Db, DataTypes)
 const sync = ()=>{
